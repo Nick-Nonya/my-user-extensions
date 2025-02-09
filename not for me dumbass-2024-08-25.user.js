1ef714cc5e1058f85d7f4efdb5f2cefc0bb1eb4a
Nick <41269331+Nick-Nonya@users.noreply.github.com>
2025-02-09 10:27:45 +0100

    Update not for me dumbass-2024-08-25.user.js

--- not for me dumbass-2024-08-25.user.js
+++ not for me dumbass-2024-08-25.user.js
@@ -2,10 +2,16 @@
 // @name         not for me dumbass
 // @namespace    http://tampermonkey.net/
 // @version      2024-08-25
-// @description  fuck off
-// @author       You
-// @match        https://www.tumblr.com/dashboard/stuff_for_you
+// @description  redirects away from the for you page
+// @author       nicknonya
+// @match        https://www.tumblr.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tumblr.com
 // ==/UserScript==
 
-location.href = 'https://www.tumblr.com/dashboard/following'
+function forYouRedirect() {
+  if (location.href.includes('dashboard/stuff_for_you')) {
+    location.href = 'https://www.tumblr.com/dashboard/following'
+  }
+}
+
+setInterval(forYouRedirect, 100);
